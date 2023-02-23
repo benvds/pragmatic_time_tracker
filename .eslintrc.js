03e1cc5035c08dccb101c0e19c4d848424846979
benvds <info@getlean.digital>
2023-02-23 19:19:25 +0100

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+        "import/resolver": {
+            node: {
+                paths: ["src"],
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+            },
+        },
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
+    extends: [
+        // By extending from a plugin config, we can get recommended rules without having to add them manually.
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:import/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:@typescript-eslint/recommended",
+        // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+        // Make sure it's always the last config, so it gets the chance to override other configs.
+        "eslint-config-prettier",
+    ],
+    settings: {
+        react: {
+            // Tells eslint-plugin-react to automatically detect the version of React to use.
+            version: "detect",
+        },
+        // Tells eslint how to resolve imports
+        "import/resolver": {
+            node: {
+                paths: ["src"],
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+            },
+        },
+    },
+    rules: {
+        "react/react-in-jsx-scope": "off",
+    },
+};
