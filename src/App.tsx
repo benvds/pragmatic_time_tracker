460017b9080daec126b7bdc534ba53580592d896
Ben van de Sande <info@getlean.digital>
2023-04-27 22:23:04 +0200

    kick-off first screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,32 @@ export const App = () => {
     console.debug("rendering app");
     return (
         <div className={styles.container}>
-            <form>
+            {/* <Button solid onClick={console.debug}></Button> */}
+            <form className={styles.form}>
                 <div className={styles.formField}>
                     <label htmlFor="desc">Description</label>
-                    <input type="text" name="desc" />
+                    <input type="text" name="desc" tabIndex={0} />
+                </div>
+                <div className={styles.formField}>
+                    <label htmlFor="project">Project</label>
+                    <input type="text" name="project" tabIndex={0} />
+                </div>
+                <div className={styles.formField}>
+                    <label htmlFor="project">Duration (hh:mm)</label>
+                    <div className={styles.inputGroup}>
+                        <input
+                            className={styles.doubleDigits}
+                            type="number"
+                            name="hh"
+                            tabIndex={0}
+                        />
+                        <input
+                            className={styles.doubleDigits}
+                            type="number"
+                            name="mm"
+                            tabIndex={0}
+                        />
+                    </div>
                 </div>
             </form>
         </div>
