acad12990d650406d516bbf7e4848ca9733bfe3b
benvds <info@getlean.digital>
2025-09-07 21:34:38 +0200

    update vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="vitest/config" />
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -7,15 +8,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    // test: {
-    //     globals: true,
-    //     environment: "happy-dom",
-    //     setupFiles: "./test/vitest.setup.ts",
-    //     coverage: {
-    //         provider: "c8",
-    //     },
-    //     // you might want to disable it, if you don't have tests that rely on CSS
-    //     // since parsing CSS is slow
-    //     // css: true,
-    // },
+    test: {
+        globals: true,
+        environment: "happy-dom",
+        setupFiles: "./test/vitest.setup.ts",
+        // you might want to disable it, if you don't have tests that rely on CSS
+        // since parsing CSS is slow
+        // css: true,
+    },
 });
