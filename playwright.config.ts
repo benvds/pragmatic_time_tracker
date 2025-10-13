bddbb9987b633bbb1552d604da4f74da111cb1b1
benvds <info@getlean.digital>
2025-10-13 13:20:56 +0200

    update playwright dev server to not build before start

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: "pnpm build && pnpm preview",
+        command: "pnpm preview",
         url: "http://localhost:4173",
         reuseExistingServer: !process.env.CI,
     },
